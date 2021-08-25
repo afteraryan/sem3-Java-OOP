f8d0b07bba131318f4c1f4be17ec70384893edd0
afteraryan <saxenaa039@gmail.com>
2021-08-25 18:57:33 +0530

    ..

---
+++ LAB/Week 3/Experiment 3/Divisibleby5.java
@@ -0,0 +1,11 @@
+public class Divisibleby5 {
+    public static void main(String[] args)
+    {
+        int sum=0;
+        for (int i = 40; i <=250; i+=5)
+        {
+            sum+=i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
