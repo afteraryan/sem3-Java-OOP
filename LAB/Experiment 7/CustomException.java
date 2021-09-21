2ed61ba7b6b38a5affe5711534ee3905258102eb
afteraryan <saxenaa039@gmail.com>
2021-09-21 10:36:05 +0530

    Experiment 7 Question 2 UserDefinesExceptioClass

---
+++ LAB/Experiment 7/CustomException.java
@@ -0,0 +1,40 @@
+class UserException extends Exception
+{
+    public UserException(String s)
+    {
+        super(s);
+    }
+}
+  
+public class CustomException
+{
+    public static boolean isNumeric(String str)
+    {
+        return str != null && str.matches("[-+]?\\d*\\.?\\d+");
+    }
+
+    public static void main(String args[])
+    {
+        
+        try
+        {
+            if(!isNumeric(args[0])||!isNumeric(args[1]))
+            {
+                throw new UserException("Non-Numeric Entry!");
+                
+            }    
+            else
+            {
+                int a = Integer.parseInt(args[0]);
+                int b = Integer.parseInt(args[1]);
+                System.out.println(a%b);
+                
+            }
+        }
+        catch (UserException e)
+        {
+            System.out.println("Excepion Caught!");
+            System.out.println(e.getMessage() + "\n");
+        }
+    }
+}
