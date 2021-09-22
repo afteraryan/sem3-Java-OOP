c05f9f999beb73f235dee3ff6f8d3a43c4ac0332
afteraryan <saxenaa039@gmail.com>
2021-09-22 11:48:07 +0530

    Experiment 7.4 InputOutputException

---
+++ LAB/Experiment 7/InputOutputException.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class InputOutputException {
+    public static void main(String[] args)
+    {
+        int a,b;
+        try
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Input a & b: ");
+            a= sc.nextInt();
+            b= sc.nextInt();
+            sc.close();
+            try
+            {
+                System.out.println(a/b);
+            }
+            catch(ArithmeticException e)
+            {
+                System.out.println(e);
+            }
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.println(e);
+        }
+        
+    }
+}
