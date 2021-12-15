5a1395b9111be012028a9485ed3d380d090fa0ec
afteraryan <saxenaa039@gmail.com>
2021-12-15 16:20:16 +0530

    BabluNotAppropriate

--- LAB/Experiment 4/Exp4.java
+++ LAB/Experiment 4/Exp4.java
@@ -1,6 +1,7 @@
 //Consider the trunk calls of a telephone exchange. A trunk call can be ordinary, urgent or lightning. 
 //The charges depend on the duration and the type of the call. 
 //Write a program using the concept of polymorphism in Java to calculate the charges.
+
 class TrunkCall {
     protected int duration;
 
