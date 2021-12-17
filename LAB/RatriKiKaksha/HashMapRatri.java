3b249833b14405d8f387465b9007b32ebb6a2dbc
afteraryan <saxenaa039@gmail.com>
2021-12-17 19:37:21 +0530

    HashMapRatri

--- LAB/RatriKiKaksha/HashMapRatri.java
+++ LAB/RatriKiKaksha/HashMapRatri.java
@@ -1,6 +1,18 @@
 import java.util.*;
 public class HashMapRatri 
 {
-    HashMap<String,Integer> hm = new HashMap<String,Integer>();
-
+    public static void main(String[] args)
+    {
+        HashMap<String,Integer> hm = new HashMap<String,Integer>();
+        hm.put("Aryan",22);
+        hm.put("Arya",21);
+        hm.put("Ary",25);
+        //hm.clear();
+        HashMap<String, Integer> hm2 = new HashMap<String,Integer>();
+        hm2.put("Riya",1);
+        hm2.put("Riy",2);
+        hm.putAll(hm2);
+        System.out.println(hm);
+    }
+    
 }
